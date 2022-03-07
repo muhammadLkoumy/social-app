46986061d63519f988d7409f46e79ed54166c66d
muhammadLkoumy <muhammadlkoumy@gmail.com>
2022-03-07 02:30:04 +0200

    screenshots

---
+++ lib/shared/styles/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+final MaterialColor myColor = const MaterialColor(0xFF262626, <int, Color>{
+  50: Color(0xFF262626),
+  100: Color(0xFF262626),
+  200: Color(0xFF262626),
+  300: Color(0xFF262626),
+  400: Color(0xFF262626),
+  500: Color(0xFF262626),
+  600: Color(0xFF262626),
+  700: Color(0xFF262626),
+  800: Color(0xFF262626),
+  900: Color(0xFF262626),
+});
